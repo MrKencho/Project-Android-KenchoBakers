4c54b32e3bd7bfafda6cce962c6ebb5fbedce497
Rishabh Saini <RishabhSaini2@gmail.com>
2017-03-19 11:39:01 +0530

    Add files via upload

---
+++ app/src/main/java/com/mrkencho/kenchobakers/KenchoBakers.java
@@ -0,0 +1,13 @@
+package com.mrkencho.kenchobakers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KenchoBakers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kencho_bakers);
+    }
+}
